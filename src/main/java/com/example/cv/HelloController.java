c100590cc5bd93bcf9d8a590104bb7613e63b0f7
hc98hirobi <hc98hirobi@gmail.com>
2025-11-17 14:40:49 +0600

    scene change by button generate cv

--- src/main/java/com/example/cv/HelloController.java
+++ src/main/java/com/example/cv/HelloController.java
@@ -1,14 +1,47 @@
 package com.example.cv;
 
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
+    /*public void switch_to_scene1(ActionEvent event) throws IOException {
+        Parent root =FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }*/
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void switch_to_scene2(ActionEvent event) throws IOException {
+        /*Parent root =FXMLLoader.load(HelloApplication.class.getResource("hello-view2.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        */
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
     }
+
+
 }
