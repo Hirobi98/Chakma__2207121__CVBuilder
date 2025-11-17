6da462cde050205949cb12ca67fcaf8b3af5d470
hc98hirobi <hc98hirobi@gmail.com>
2025-11-17 22:56:55 +0600

    designed second and 3rd page

--- src/main/java/com/example/cv/HelloController.java
+++ src/main/java/com/example/cv/HelloController.java
@@ -40,6 +40,66 @@ public class HelloController {
     @FXML
     private VBox workExperienceContainer;
 
+    public static class educationentry{
+        String degree;
+        String institution;
+        String year;
+    }
+    public static class workexperienceentry{
+        String company_name;
+        String period;
+        String job_title;
+
+
+    }
+    @FXML
+    public void addnewedu(ActionEvent event){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("educationentry.fxml"));
+            fxmlLoader.setController(this);
+            Node eduentry=fxmlLoader.load();
+            educationContainer.getChildren().add(eduentry);
+
+
+
+        }
+        catch(IOException e){
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+
+        }
+
+    }
+    @FXML
+    public void addnewworkexperience(ActionEvent event){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("workexperienceentry.fxml"));
+            fxmlLoader.setController(this);
+            Node workentry=fxmlLoader.load();
+            educationContainer.getChildren().add(workentry);
+
+
+
+        }
+        catch(IOException e){
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+
+        }
+
+    }
+    @FXML
+    public void removeEntry(ActionEvent event) {
+
+        Node source = (Node) event.getSource();
+        Node entryToRemove = source.getParent();
+
+
+        if (entryToRemove.getParent() instanceof VBox) {
+            ((VBox)entryToRemove.getParent()).getChildren().remove(entryToRemove);
+        }
+    }
+
     public void switch_to_scene1(ActionEvent event) throws IOException {
         Parent root =FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
         stage=(Stage)((Node)event.getSource()).getScene().getWindow();
@@ -48,18 +108,10 @@ public class HelloController {
         stage.show();
 
     }
-    @FXML
-    public void addnewedu(ActionEvent event) throws IOException {
 
 
 
-    }
-    @FXML
-    public void addnewworkexperience(ActionEvent event) throws IOException {
 
-
-
-    }
     @FXML
     public void switch_to_scene2(ActionEvent event) throws IOException {
         /*Parent root =FXMLLoader.load(HelloApplication.class.getResource("hello-view2.fxml"));
