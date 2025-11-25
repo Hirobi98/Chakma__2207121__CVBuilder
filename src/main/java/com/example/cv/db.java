918e2aadd6de069085deb51ff8dae1e53c5bffe6
hc98hirobi <hc98hirobi@gmail.com>
2025-11-25 15:22:59 +0600

    created db tables

--- src/main/java/com/example/cv/db.java
+++ src/main/java/com/example/cv/db.java
@@ -2,6 +2,7 @@ package com.example.cv;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
@@ -18,10 +19,33 @@ public class db {
             if(connection==null || connection.isClosed()){
                 connection= DriverManager.getConnection("jdbc:sqlite:cv.db");
                 logger.info("Connected to database successfully");
+                create_table();
 
             }
 
         } catch (SQLException e){
+            logger.info(e.toString());
+        }
+    }
+
+    public void create_table(){
+        get_connection();
+        String entry="CREATE TABLE IF NOT EXISTS cv_entries ("
+                + "id INTEGER PRIMARY KEY,"
+                + "fullName TEXT NOT NULL,"
+                + "email TEXT,"
+                + "phone TEXT,"
+                + "address TEXT,"
+                + "skills TEXT,"
+                + "projects TEXT,"
+                + "education TEXT,"
+                + "workExperience TEXT"
+                + ");";
+        try(PreparedStatement statement = connection.prepareStatement(entry)){
+            statement.execute();
+            logger.info("table created successfully");
+
+        }catch(SQLException e){
             logger.info(e.toString());
         }
     }
